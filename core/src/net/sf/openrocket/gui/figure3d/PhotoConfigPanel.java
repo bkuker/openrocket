b29dff0ad2cc4811a02a11f598cd68966794b7dd
bkuker <bkuker@billkuker.com>
2013-09-01 11:31:01 -0400

    Use tabs

--- core/src/net/sf/openrocket/gui/figure3d/PhotoConfigPanel.java
+++ core/src/net/sf/openrocket/gui/figure3d/PhotoConfigPanel.java
@@ -14,20 +14,19 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
+import javax.swing.JTabbedPane;
 
 import net.miginfocom.swing.MigLayout;
 import net.sf.openrocket.gui.adaptors.BooleanModel;
 import net.sf.openrocket.gui.adaptors.DoubleModel;
 import net.sf.openrocket.gui.components.ColorIcon;
-import net.sf.openrocket.gui.components.StyledLabel;
-import net.sf.openrocket.gui.components.StyledLabel.Style;
 import net.sf.openrocket.gui.components.UnitSelector;
 import net.sf.openrocket.gui.util.ColorConversion;
 import net.sf.openrocket.startup.Application;
 import net.sf.openrocket.unit.UnitGroup;
 import net.sf.openrocket.util.StateChangeListener;
 
-public class PhotoConfigPanel extends JPanel {
+public class PhotoConfigPanel extends JTabbedPane {
 	private static final JColorChooser colorChooser = new JColorChooser();
 	
 	private class ColorActionListener implements ActionListener {
@@ -69,7 +68,7 @@ public class PhotoConfigPanel extends JPanel {
 	}
 	
 	public PhotoConfigPanel(final PhotoBooth.Photo p) {
-		super(new MigLayout("fill"));
+		super();
 		
 		final JButton sunLightColorButton = new JButton();
 		sunLightColorButton.setMaximumSize(new Dimension(35, 25));
@@ -102,105 +101,121 @@ public class PhotoConfigPanel extends JPanel {
 		flameColorButton.addActionListener(new ColorActionListener(p, "FlameColor"));
 		
 		
-		add(new StyledLabel("Rocket", Style.BOLD), "wrap");
 		
-		add(new JLabel("Pitch"));
-		DoubleModel pitchModel = new DoubleModel(p, "Pitch", UnitGroup.UNITS_ANGLE);
-		add(new JSpinner(pitchModel.getSpinnerModel()), "w 40");
-		add(new UnitSelector(pitchModel), "wrap");
-		
-		add(new JLabel("Yaw"));
-		DoubleModel yawModel = new DoubleModel(p, "Yaw", UnitGroup.UNITS_ANGLE);
-		add(new JSpinner(yawModel.getSpinnerModel()), "w 40");
-		add(new UnitSelector(yawModel), "wrap");
-		
-		add(new JLabel("Roll"));
-		DoubleModel rollModel = new DoubleModel(p, "Roll", UnitGroup.UNITS_ANGLE);
-		add(new JSpinner(rollModel.getSpinnerModel()), "w 40");
-		add(new UnitSelector(rollModel), "wrap");
-		
-		add(new JLabel("Advance"));
-		DoubleModel advanceModel = new DoubleModel(p, "Advance", UnitGroup.UNITS_LENGTH);
-		add(new JSpinner(advanceModel.getSpinnerModel()), "w 40");
-		add(new UnitSelector(advanceModel), "wrap");
-		
-		
-		
-		add(new StyledLabel("Camera", Style.BOLD), "wrap");
-		
-		add(new JLabel("View Azimuth"));
-		DoubleModel viewAzModel = new DoubleModel(p, "ViewAz", UnitGroup.UNITS_ANGLE);
-		add(new JSpinner(viewAzModel.getSpinnerModel()), "w 40");
-		add(new UnitSelector(viewAzModel), "wrap");
-		
-		add(new JLabel("View Altitude"));
-		DoubleModel viewAltModle = new DoubleModel(p, "ViewAlt", UnitGroup.UNITS_ANGLE);
-		add(new JSpinner(viewAltModle.getSpinnerModel()), "w 40");
-		add(new UnitSelector(viewAltModle), "wrap");
-		
-		add(new JLabel("View Distance"));
-		DoubleModel viewDistanceModel = new DoubleModel(p, "ViewDistance", UnitGroup.UNITS_LENGTH);
-		add(new JSpinner(viewDistanceModel.getSpinnerModel()), "w 40");
-		add(new UnitSelector(viewDistanceModel), "wrap");
-		
-		add(new JLabel("FoV"));
-		DoubleModel fovModel = new DoubleModel(p, "Fov", UnitGroup.UNITS_ANGLE);
-		add(new JSpinner(fovModel.getSpinnerModel()), "w 40");
-		add(new UnitSelector(fovModel), "wrap");
-		
-		add(new StyledLabel("Light", Style.BOLD), "wrap");
-		
-		add(new JLabel("Sun Light"));
-		add(sunLightColorButton, "wrap");
-		
-		add(new JLabel("Ambiance"));
-		DoubleModel ambianceModel = new DoubleModel(p, "Ambiance", 100, UnitGroup.UNITS_NONE, 0, 100);
-		add(new JSpinner(ambianceModel.getSpinnerModel()), "wrap");
-		
-		add(new JLabel("Light Azimuth"));
-		DoubleModel lightAzModel = new DoubleModel(p, "LightAz", UnitGroup.UNITS_ANGLE);
-		add(new JSpinner(lightAzModel.getSpinnerModel()), "w 40");
-		add(new UnitSelector(lightAzModel), "wrap");
-		
-		add(new JLabel("Light Altitude"));
-		DoubleModel lightAltModle = new DoubleModel(p, "LightAlt", UnitGroup.UNITS_ANGLE);
-		add(new JSpinner(lightAltModle.getSpinnerModel()), "w 40");
+		addTab("Rocket", new JPanel(new MigLayout("fill")) {
+			{
+				add(new JLabel("Pitch"));
+				DoubleModel pitchModel = new DoubleModel(p, "Pitch", UnitGroup.UNITS_ANGLE);
+				add(new JSpinner(pitchModel.getSpinnerModel()), "w 40");
+				add(new UnitSelector(pitchModel), "wrap");
+				
+				add(new JLabel("Yaw"));
+				DoubleModel yawModel = new DoubleModel(p, "Yaw", UnitGroup.UNITS_ANGLE);
+				add(new JSpinner(yawModel.getSpinnerModel()), "w 40");
+				add(new UnitSelector(yawModel), "wrap");
+				
+				add(new JLabel("Roll"));
+				DoubleModel rollModel = new DoubleModel(p, "Roll", UnitGroup.UNITS_ANGLE);
+				add(new JSpinner(rollModel.getSpinnerModel()), "w 40");
+				add(new UnitSelector(rollModel), "wrap");
+				
+				add(new JLabel("Advance"));
+				DoubleModel advanceModel = new DoubleModel(p, "Advance", UnitGroup.UNITS_LENGTH);
+				add(new JSpinner(advanceModel.getSpinnerModel()), "w 40");
+				add(new UnitSelector(advanceModel), "wrap");
+				
+			}
+		});
 		
 		
-		add(new StyledLabel("Sky", Style.BOLD), "wrap");
-		
-		add(new JLabel("Sky Color"));
-		add(skyColorButton, "wrap");
-		
-		add(new JLabel("Sky Image"));
-		add(new JCheckBox(new BooleanModel(p, "SkyEnabled")), "wrap");
 		
 		
-		add(new StyledLabel("Exhaust", Style.BOLD), "wrap");
+		addTab("Camera", new JPanel(new MigLayout("fill")) {
+			{
+				
+				add(new JLabel("View Azimuth"));
+				DoubleModel viewAzModel = new DoubleModel(p, "ViewAz", UnitGroup.UNITS_ANGLE);
+				add(new JSpinner(viewAzModel.getSpinnerModel()), "w 40");
+				add(new UnitSelector(viewAzModel), "wrap");
+				
+				add(new JLabel("View Altitude"));
+				DoubleModel viewAltModle = new DoubleModel(p, "ViewAlt", UnitGroup.UNITS_ANGLE);
+				add(new JSpinner(viewAltModle.getSpinnerModel()), "w 40");
+				add(new UnitSelector(viewAltModle), "wrap");
+				
+				add(new JLabel("View Distance"));
+				DoubleModel viewDistanceModel = new DoubleModel(p, "ViewDistance", UnitGroup.UNITS_LENGTH);
+				add(new JSpinner(viewDistanceModel.getSpinnerModel()), "w 40");
+				add(new UnitSelector(viewDistanceModel), "wrap");
+				
+				add(new JLabel("FoV"));
+				DoubleModel fovModel = new DoubleModel(p, "Fov", UnitGroup.UNITS_ANGLE);
+				add(new JSpinner(fovModel.getSpinnerModel()), "w 40");
+				add(new UnitSelector(fovModel), "wrap");
+			}
+		});
 		
-		add(new JLabel("Smoke"));
-		add(new JCheckBox(new BooleanModel(p, "Smoke")), "split 2, w 15");
-		add(smokeColorButton, "wrap");
+		addTab("Light", new JPanel(new MigLayout("fill")) {
+			{
+				add(new JLabel("Sun Light"));
+				add(sunLightColorButton, "wrap");
+				
+				add(new JLabel("Ambiance"));
+				DoubleModel ambianceModel = new DoubleModel(p, "Ambiance", 100, UnitGroup.UNITS_NONE, 0, 100);
+				add(new JSpinner(ambianceModel.getSpinnerModel()), "wrap");
+				
+				add(new JLabel("Light Azimuth"));
+				DoubleModel lightAzModel = new DoubleModel(p, "LightAz", UnitGroup.UNITS_ANGLE);
+				add(new JSpinner(lightAzModel.getSpinnerModel()), "w 40");
+				add(new UnitSelector(lightAzModel), "wrap");
+				
+				add(new JLabel("Light Altitude"));
+				DoubleModel lightAltModle = new DoubleModel(p, "LightAlt", UnitGroup.UNITS_ANGLE);
+				add(new JSpinner(lightAltModle.getSpinnerModel()), "w 40");
+				
+			}
+		});
 		
-		add(new JLabel("Opacity"));
-		DoubleModel smokeAlphaModel = new DoubleModel(p, "SmokeAlpha", 100, UnitGroup.UNITS_NONE, 0, 100);
-		add(new JSpinner(smokeAlphaModel.getSpinnerModel()), "wrap");
+		addTab("Sky", new JPanel(new MigLayout("fill")) {
+			{
+				
+				add(new JLabel("Sky Color"));
+				add(skyColorButton, "wrap");
+				
+				add(new JLabel("Sky Image"));
+				add(new JCheckBox(new BooleanModel(p, "SkyEnabled")), "wrap");
+			}
+		});
 		
-		add(new JLabel("Fire"));
-		add(new JCheckBox(new BooleanModel(p, "Flame")), "split 2, w 15");
-		add(flameColorButton, "wrap");
+		addTab("Exhaust", new JPanel(new MigLayout("fill")) {
+			{
+				add(new JLabel("Smoke"));
+				add(new JCheckBox(new BooleanModel(p, "Smoke")), "split 2, w 15");
+				add(smokeColorButton, "wrap");
+				
+				add(new JLabel("Opacity"));
+				DoubleModel smokeAlphaModel = new DoubleModel(p, "SmokeAlpha", 100, UnitGroup.UNITS_NONE, 0, 100);
+				add(new JSpinner(smokeAlphaModel.getSpinnerModel()), "wrap");
+				
+				add(new JLabel("Fire"));
+				add(new JCheckBox(new BooleanModel(p, "Flame")), "split 2, w 15");
+				add(flameColorButton, "wrap");
+				
+				add(new JLabel("Sparks"));
+				add(new JCheckBox(new BooleanModel(p, "Sparks")), "wrap");
+				
+				add(new JLabel("Scale"));
+				DoubleModel exhaustScaleModel = new DoubleModel(p, "ExhaustScale", 100, UnitGroup.UNITS_NONE, 0, 1000);
+				add(new JSpinner(exhaustScaleModel.getSpinnerModel()), "wrap");
+			}
+		});
 		
-		add(new JLabel("Sparks"));
-		add(new JCheckBox(new BooleanModel(p, "Sparks")), "wrap");
+		addTab("Effects", new JPanel(new MigLayout("fill")) {
+			{
+				add(new JLabel("Speed"));
+				add(new JCheckBox(new BooleanModel(p, "MotionBlurred")), "wrap");
+			}
+		});
 		
-		add(new JLabel("Scale"));
-		DoubleModel exhaustScaleModel = new DoubleModel(p, "ExhaustScale", 100, UnitGroup.UNITS_NONE, 0, 1000);
-		add(new JSpinner(exhaustScaleModel.getSpinnerModel()), "wrap");
-		
-		
-		add(new StyledLabel("Effects", Style.BOLD), "wrap");
-		
-		add(new JLabel("Speed"));
-		add(new JCheckBox(new BooleanModel(p, "MotionBlurred")), "wrap");
 	}
 }
