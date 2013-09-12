439bd07242ddf392207d6ccf1762410e5600cd20
bkuker <bkuker@billkuker.com>
2013-09-12 11:32:02 -0400

    Fix a couple bugs in the fragment shader OSX caught. Windows was much more forgiving.

--- core/src/net/sf/openrocket/gui/figure3d/photo/exhaust/smokeShader.glsl
+++ core/src/net/sf/openrocket/gui/figure3d/photo/exhaust/smokeShader.glsl
@@ -6,12 +6,12 @@ void main(void)
 	vec3 normal = 2.0 * texture2D (uNormal, gl_TexCoord[0].st).rgb - 1.0;
 	normal = normalize (normal);
 	
-	float  lamberFactor = max (dot (gl_LightSource[1].position, normal), 0.0) ;
+	float lamberFactor = max (dot (gl_LightSource[1].position.xyz, normal), 0.0) ;
 	
 	lamberFactor = lamberFactor * .6 + .4;
 	
 	vec4 diffuseMaterial = texture2D (uSmoke, gl_TexCoord[0].st);
-	diffuseMaterial.rgb = 1;
+	diffuseMaterial.rgb = vec3(1);
 	
 	vec4 diffuseLight = gl_LightSource[1].diffuse;
 	vec4 ambientLight = gl_LightSource[1].ambient;
